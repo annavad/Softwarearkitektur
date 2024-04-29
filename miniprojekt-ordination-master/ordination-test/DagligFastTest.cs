7d3e08acfc579dcebcf837873056f177de937251
NikolaiWK <nikolaiwolhardt@gmail.com>
2024-04-29 23:01:39 +0200

    Prøvet at lave test, men db driller

--- miniprojekt-ordination-master/ordination-test/DagligFastTest.cs
+++ miniprojekt-ordination-master/ordination-test/DagligFastTest.cs
@@ -1,10 +1,27 @@
-﻿using shared.Model;
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using shared.Model;
 
 namespace ordination_test.Model;
 
 [TestClass]
 public class DagligFastTest
+
 {
+    private DataService service;
+
+
+    [TestInitialize]
+    public void SetupBeforeEachTest()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+        var context = new OrdinationContext(optionsBuilder.Options);
+        service = new DataService(context);
+        service.SeedData();
+    }
+
     [TestMethod]
     public void DoegnDosis_()
     {
@@ -27,6 +44,25 @@ public class DagligFastTest
 
     public void Paracetamol_Undervægtig()
     {
+      
+        {
+            
+            var patientId = 1; 
+            var laegemiddelId = 1; 
+            
+
+            var patient = new Patient { PatientId = patientId, vaegt = 20 }; 
+            var lægemiddel = new Laegemiddel { LaegemiddelId = laegemiddelId, enhedPrKgPrDoegnLet = 0.5 }; 
+
+            db.AddPatient(patient);
+            db.AddLægemiddel(lægemiddel);
+
+            
+            var result = db.GetAnbefaletDosisPerDøgn(patientId, laegemiddelId);
+
+            
+            Assert.AreEqual(22, result); 
+        }
 
     }
 
